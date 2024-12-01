3dbe35f889e039e7d19439c9f2f265483a146998
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2024-12-01 12:04:17 +0200

    Swap to using Arrays instead of Seq

--- Day1.fs
+++ Day1.fs
@@ -4,20 +4,20 @@ open AoC2024.Prelude
 
 
 let part1 fn () =
-    let input = readInput fn |> Seq.map (splitS "   ") |> Seq.transpose |> Seq.map Seq.sort
-    let a = Seq.head input |> Seq.map int64
-    let b = Seq.last input |> Seq.map int64
-    let pairs =  Seq.zip a b 
-    pairs |> Seq.map (fun t -> fst t - snd t |> abs) |> Seq.sum
+    let input = readInput fn |> Seq.map (splitS "   ") |> Array.transpose |> Array.map Array.sort
+    let a = Array.head input |> Array.map int64
+    let b = Array.last input |> Array.map int64
+    let pairs =  Array.zip a b 
+    pairs |> Array.map (fun t -> fst t - snd t |> abs) |> Array.sum
    
 let part2 fn () = 
-    let input = readInput fn |> Seq.map (splitS "   ") |> Seq.transpose 
-    let a = Seq.head input |> Seq.map int64
-    let b = Seq.last input |> Seq.map int64
-    let counts = b |> Seq.countBy id |> Map.ofSeq
+    let input = readInput fn |> Seq.map (splitS "   ") |> Array.transpose 
+    let a = Array.head input |> Array.map int64
+    let b = Array.last input |> Array.map int64
+    let counts = b |> Array.countBy id |> Map.ofArray
     let getCount n =
         match counts |> Map.tryFind n with
         | Some c -> int64 c
         | None -> 0L
-    a |> Seq.map (fun n -> getCount n * n) |> Seq.sum
+    a |> Array.map (fun n -> getCount n * n) |> Array.sum
 

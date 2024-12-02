5b554868dd2e455f4fa56b3d78a71657cf4524f6
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2024-12-02 14:44:28 +0200

    Solve day 2, part 1

---
+++ Day2.fs
@@ -0,0 +1,37 @@
+﻿module AoC2024.Day2
+
+open AoC2024.Prelude
+
+type Pair =
+    | Inc
+    | Dec
+    | Invalid
+
+let compare t =
+    match fst t - snd t with
+    | -1L
+    | -2L
+    | -3L -> Dec
+    | 1L
+    | 2L
+    | 3L -> Inc
+    | _ -> Invalid
+
+let test arr =
+    Array.contains Invalid arr |> not && Array.length arr = 1
+
+let part1 fn () =
+    let input =
+        readInput fn
+        |> Seq.map (split ' ')
+        |> Array.ofSeq
+        |> Array.map (Array.map int64 >> Array.pairwise)
+
+    input
+    |> Array.map (fun arr -> arr |> Array.map compare |> Array.groupBy id |> Array.map fst)
+    |> Array.filter test
+    |> Array.length
+    |> int64
+
+
+let part2 fn () = 0L
--- Program.fs
+++ Program.fs
@@ -16,9 +16,9 @@ let main argv =
     match day with
     | "1" -> Day1.part1 "1" ()
     | "1b" -> Day1.part2 "1" ()
-    (*
     | "2" -> Day2.part1 "2" ()
     | "2b" -> Day2.part2 "2" ()
+    (*
     | "3" -> Day3.part1 "3" ()
     | "3b" -> Day3.part2 "3" ()
     | "4" -> Day4.part1 "4" ()
@@ -71,7 +71,7 @@ let main argv =
     | "25" -> Day25.part1 "25" ()
     | "25b" -> Day25.part2 "25" ()
     *)
-    | "test" -> Day1.part1 "test1" ()
+    | "test" -> Day2.part1 "test2" ()
     |> printfn "%d"
     stopWatch.Stop()
     printfn "Ran for %0.3f seconds" stopWatch.Elapsed.TotalSeconds

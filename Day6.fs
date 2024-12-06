7587fd4a9e0a99125265e02fd3b06a320919d819
Kimmo Parviainen-Jalanko <kimmo@vainu.io>
2024-12-06 17:21:03 +0200

    Attempt 2 for Day 6 part 2: a failure

--- Day6.fs
+++ Day6.fs
@@ -8,7 +8,7 @@ type Direction =
     | South
     | West
 
-type Guard = { Facing: Direction; X: int; Y: int; Visited: (int * int) list }
+type Guard = { Facing: Direction; X: int; Y: int; Visited: Guard list; Loops: (int*int) list }
 
 type Position =
     | Guard of Guard
@@ -34,17 +34,17 @@ let extractPositions (positions: seq<Position>) =
 
 let turn g =
     match g.Facing with
-    | North -> {g with Facing=East}
-    | East -> {g with Facing=South}
-    | South -> {g with Facing=West}
-    | West -> {g with Facing=North}
-    
-let solve fn=
+    | North -> { g with Facing = East }
+    | East -> { g with Facing = South }
+    | South -> { g with Facing = West }
+    | West -> { g with Facing = North }
+
+let solve fn =
     let input = readInput fn
 
     let maxy = input |> Seq.length
     let maxx = input |> Seq.head |> Seq.length
-    
+
     let state =
         input
         |> Seq.mapi (fun y row ->
@@ -53,12 +53,17 @@ let solve fn=
                 match pos with
                 | '.' -> None
                 | '#' -> Some(Obstacle(x, y))
-                | '^' -> Some(Guard { X = x; Y = y; Visited = List.singleton (x,y); Facing = North })))
+                | '^' ->
+                    let g = { X = x; Y = y; Visited = List.empty; Facing = North; Loops=[]}
+                    Some(Guard { X = x; Y = y; Visited = [ g ]; Facing = North; Loops =[] })))
         |> Seq.concat
         |> Seq.choose id
-        
+
     let guard, obstacles = extractPositions state
     
+    let checkForLoop g =
+       g.Visited |> List.exists (fun g' -> g'.X = g.X && g'.Y = g.Y && g'.Facing = (turn g).Facing)
+       
     let move g =
         let x, y =
             match g.Facing with
@@ -66,23 +71,30 @@ let solve fn=
             | East -> g.X + 1, g.Y
             | South -> g.X, g.Y + 1
             | West -> g.X - 1, g.Y
-        match Set.contains (x,y) obstacles with
+
+        match Set.contains (x, y) obstacles with
         | true -> turn g
-        | false -> {g with X=x; Y=y}
-        
+        | false ->
+            match checkForLoop g with
+            | false -> { g with X = x; Y = y }
+            | true -> {g with X = x; Y = y; Loops = g.Loops @ [x,y]}
+
     let rec patrol g =
         let g' = move g
+
         match g'.X, g'.Y with
-        | x,y when x < 0 || y < 0 || x >= maxx || y >= maxy -> g
-        | x,y -> patrol {g' with Visited=g.Visited @ [(x,y)]}
+        | x, y when x < 0 || y < 0 || x >= maxx || y >= maxy -> g
+        | _ -> patrol { g' with Visited = g.Visited @ [ g' ] }
+
     patrol guard
 
+let getVisitedCoords g = g.Visited |> List.map (fun v -> v.X, v.Y)
+
 let part1 fn () =
-    let finish = solve fn
-    finish.Visited |> Set.ofSeq |> Set.count |> int64
-    
+    let guard = solve fn
+    getVisitedCoords guard |> Set.ofSeq |> Set.count |> int64
+
 let part2 fn () =
-    let finish = solve fn
-    finish.Visited |> List.groupBy id |> List.filter (fun l -> List.length (snd l) > 1) |> printfn "%A"
-    let unique = finish.Visited |> Set.ofSeq |> Set.count
-    unique |> int64
+    let guard = solve fn
+    printfn "%A" guard.Loops
+    guard.Loops |>List.length |> int64
